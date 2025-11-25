97fc30baa18b37cf9956fd7fed35046209d0283e
JiaminLo <jluo6565@conestogac.on.ca>
2025-11-25 16:07:09 -0500

    chore: update company images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import Allegis from "./company/Allegis_Group.jpg";
+import isoftstone from "./company/isoftstone.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  Allegis,
+  isoftstone,
   carrent,
   jobit,
   tripguide,
